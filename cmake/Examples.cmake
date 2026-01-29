04af7770f6d55ac0bb7a2658dcc2e63fdbd42087
WrathfulSpatula <stranoj@gmail.com>
2026-01-28 19:07:46 -0500

    nand -> xor

--- cmake/Examples.cmake
+++ cmake/Examples.cmake
@@ -1,8 +1,8 @@
-add_executable (nand
-    examples/nand.cpp
+add_executable (xor
+    examples/xor.cpp
     )
-set_target_properties(nand PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/examples")
-target_link_libraries (nand ${WEED_LIBS})
+set_target_properties(xor PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/examples")
+target_link_libraries (xor ${WEED_LIBS})
 
 set(EXAMPLE_COMPILE_OPTS ${TEST_COMPILE_OPTS} -DCATCH_CONFIG_FAST_COMPILE)
 target_compile_options (nand PUBLIC ${EXAMPLE_COMPILE_OPTS})
