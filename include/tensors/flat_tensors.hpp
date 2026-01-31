fb5dd0bae098e83fac6d8e686b43eb3776478e1d
WrathfulSpatula <stranoj@gmail.com>
2026-01-30 21:12:08 -0500

    const size_t n

--- include/tensors/flat_tensors.hpp
+++ include/tensors/flat_tensors.hpp
@@ -21,26 +21,26 @@
 #define CPU_INIT_2_SCALAR(ft, strg)                                            \
   GET_CONST_FLAT_TENSOR(ft, a, pa);                                            \
   GET_STORAGE(strg, out, po);                                                  \
-  size_t n = a.get_broadcast_size()
+  const size_t n = a.get_broadcast_size()
 
 #define CPU_INIT_2(ft, strg)                                                   \
   GET_CONST_FLAT_TENSOR(ft, a, pa);                                            \
   GET_STORAGE(strg, out, po);                                                  \
-  size_t n = out.storage->size
+  const size_t n = out.storage->size
 
 #define CPU_INIT_2_IN_PLACE(ft1, ft2)                                          \
   GET_FLAT_TENSOR(ft1, a, pa);                                                 \
   GET_CONST_FLAT_TENSOR(ft2, b, pb);                                           \
-  size_t n = a.get_broadcast_size()
+  const size_t n = a.get_broadcast_size()
 
 #define CPU_INIT_3(ft1, ft2, strg)                                             \
   GET_CONST_FLAT_TENSOR(ft1, a, pa);                                           \
   GET_CONST_FLAT_TENSOR(ft2, b, pb);                                           \
   GET_STORAGE(strg, out, po);                                                  \
-  size_t n = out.storage->size
+  const size_t n = out.storage->size
 
 #define CPU_GRAD_INIT_3(ft1, ft2, ft3)                                         \
   GET_FLAT_TENSOR(ft1, din, pdi);                                              \
   GET_CONST_FLAT_TENSOR(ft2, in, pi);                                          \
   GET_CONST_FLAT_TENSOR(ft3, dout, po);                                        \
-  size_t n = din.get_broadcast_size()
+  const size_t n = din.get_broadcast_size()
